e560856bbf5151bb182cb757e0d588d772f4af05
tamupino <pferrer@hksinc.com>
2018-10-29 16:56:44 -0500

    Just getting back into things after a break... Fixed the delete resources script Started working on a new Bat2Shell function - added it to a helper file Creating a "Run DGB" script to host just the calcs needed to run DGP

---
+++ HELPERS/HELPER_BatConversions.py
@@ -0,0 +1,22 @@
+
+class Convert:
+    def __init__(self):
+        self.nothing = None
+
+    def bat_to_sh_DGP(self, file_path):
+        sh_file = file_path[:-4] + '.sh'
+        with open(file_path, 'r') as infile, open(sh_file, 'w') as outfile:
+            outfile.write('#!/usr/bin/env bash\n\n')
+            for i,line in enumerate(infile):
+                if i <=4:
+                    pass
+                else:
+                    outfile.write(line + '\n')
+            outfile.close()
+
+
+
+
+path = r"C:\Users\pferrer\Desktop\test\21MAR600_IMGInit.bat"
+Convert().bat_to_sh_DGP(path)
+
--- HKS_DeleteAzureResources.py
+++ HKS_DeleteAzureResources.py
@@ -1,9 +1,10 @@
-import time
+
 from HELPERS.HELPER_Login_Info import Login
 from HELPERS.HELPER_SMS import SMS
 import HELPERS.HELPER_Management_Clients
 import _CORE_AUTOBUNTU as core
 import os
+
 
 
 ################################################# Run Code #############################################################
@@ -18,14 +19,29 @@ if Delete_VMs:
     # Delete the resource group
     # Instantiate the management client class
     mgmt = HELPERS.HELPER_Management_Clients.MGMT(Login)
-    core.delete_resources(mgmt.resource_group_client(Login),Login)
+    client = mgmt.resource_group_client(Login)
+    for item in client.resource_groups.list():
+        if item.name == Login.GROUP_NAME:
+            core.delete_resources(mgmt.resource_group_client(Login),Login)
 
-    # Send the SMS Notification
-    # Instantiate the SMS class
-    sms = SMS()
-    sms.DeleteResources(Login)
-    os.remove(os.path.join(os.getcwd(), 'HELPERS', 'Local_IP_Addresses.py'))
-
+            # Send the SMS Notification
+            # Instantiate the SMS class
+            sms = SMS()
+            sms.DeleteResources(Login)
+            IP_List_File = os.path.join(os.getcwd(), 'HELPERS', 'Local_IP_Addresses.py')
+            if os.path.isfile(IP_List_File):
+                os.remove(IP_List_File)
+                print("IP File Found and Deleted")
+            else:
+                print("IP File Not Detected")
+            print("Resource Group Deleted")
+        else:
+            IP_List_File = os.path.join(os.getcwd(), 'HELPERS', 'Local_IP_Addresses.py')
+            if os.path.isfile(IP_List_File):
+                os.remove(IP_List_File)
+                print("IP File Found and Deleted")
+            else:
+                print("IP File Not Detected")
 
 else:
     print("Just Testing Stuff")
