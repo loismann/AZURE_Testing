ab373dbb1bd3e86f526492d3214e1c34ac36ac99
tamupino <pferrer@hksinc.com>
2019-01-08 17:38:47 -0600

    Started work on the Grid-based annual daylight cloud workflow. The first build out will focus on replicated Sarith's shoebox model run from the recorded videos using a single machine The second build out will scale up to SFO sized and attempt to implement Sarith's Lazy loading when the matrix calc files are too large to read The third build out will focus on splitting up the run into multiple machines

--- PF_SingleMachineGridBasedDaylightCloudWorkflowTesting/HKS_PrepAGBD.py
+++ PF_SingleMachineGridBasedDaylightCloudWorkflowTesting/HKS_PrepAGBD.py
@@ -16,6 +16,43 @@ import zipfile
 from pathlib import Path
 import random
 
+
+##########################################  DEFINE THE HELPER FUNCTIONs  ###############################################
+
+# This will get the number of VM's in use from the local copy of the IP Address file
+def GET_VMCount():
+    vm_count = 0
+    try:
+        scriptdirectory = os.path.dirname(__file__)  # <-- Absolute path of this file
+        path = os.path.join(scriptdirectory, "HELPERS/Local_IP_Addresses.py")
+        with open(path) as IP:
+            for line in IP:
+                if "IP" in line:
+                    vm_count += 1
+
+    except:
+        print("No copy of 'Local_IP_Addresses.py' found. Are VM's active?")
+    return vm_count
+
+
+# This will get the VM IP addresses as a list
+def GET_VMIP():
+    IP_Addresses = []
+    try:
+        scriptdirectory = os.path.dirname(__file__)  # <-- Absolute path of this file
+        path = os.path.join(scriptdirectory, "Local_IP_Addresses.py")
+        with open(path) as IP:
+            for line in IP:
+                if "IP" in line:
+                    words = line.split()
+                    for word in words:
+                        if "IP" not in word and "=" not in word:
+                            IP_Addresses.append(word)
+    except:
+        print("No copy of 'Local_IP_Addresses.py' found. Are VM's active?")
+    return IP_Addresses
+
+
 # This function will identify the base files that will be transferred to the cloud
 def getFilesForTransfer(Local_Main_Directory_AGBD):
     Files_For_Transfer = []
@@ -31,12 +68,18 @@ def getFilesForTransfer(Local_Main_Directory_AGBD):
 
 # Zip the identified files
 def zipFiles(Local_Main_Directory_AGBD,filesForRemoteTransfer):
-    print("This is how many files we're transferring: " + str(len(filesForRemoteTransfer)))
+    # print("This is how many files we're transferring: " + str(len(filesForRemoteTransfer)))
     os.chdir(Local_Main_Directory_AGBD)
     with zipfile.ZipFile("Transfer" + ".zip", "w") as zip:
         for file in filesForRemoteTransfer:
             zip.write(file,os.path.basename(file), compress_type=zipfile.ZIP_DEFLATED)
 
+    zipped_file = None
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".zip"):
+            zipped_file = file
+
+    return os.path.abspath(zipped_file)
 
 # This performs all operations to get the files over to azure and then run them
 # THIS WAS COPIED FROM THE DGP ANALYSIS FILES.  REMOVE THIS MESSAGE WHEN PROPERLY MODIFIED TO RUN ANNUAL GRID BASED DAYLIGHT CALCS
@@ -45,10 +88,10 @@ def sendFilesToAzureAndLaunch(Azure_Main_Directory,
                               zipFilesForRemoteTransfer,
                               ):
 
-    sleepTime = random.randint(0,10)
+    sleepTime = random.randint(0,2)
     print("randomly sleeping for " + str(sleepTime) + " seconds before beginning file transfer")
     time.sleep(sleepTime)
-
+    #
     IP = vm_IP_List
     login = Login()
     ssh = pf_ssh(IP, 22, login.ADMIN_NAME, login.ADMIN_PSWD)
@@ -59,32 +102,46 @@ def sendFilesToAzureAndLaunch(Azure_Main_Directory,
     # Might need to clear out the home directory first to help with testing:
     # Check and see if the "new" folder exists
     # if it does, delete it and then make it again
-    print(IP)
-    print(Azure_Main_Directory)
+    # print(IP)
+    # print(Azure_Main_Directory)
 
     ssh.sendCommand("mkdir new")
     original_file_path = zipFilesForRemoteTransfer
     destination_file_path = Azure_Main_Directory + r"/" + os.path.split(original_file_path)[1]
     ssh.copyfilesSCP(IP, 22, login.ADMIN_NAME, login.ADMIN_PSWD, original_file_path, destination_file_path)
-
-
-    # Copy the "HKS_LaunchDGP.py" file to the remote system
-    original_file_path = os.path.join(login.Local_Repo_Directory,HKS_LaunchDGP)
-    destination_file_path = Azure_Main_Directory + "/" + HKS_LaunchDGP
-    ssh.copyfilesSCP(IP, 22, login.ADMIN_NAME, login.ADMIN_PSWD, original_file_path, destination_file_path)
-    print(destination_file_path + " Copied to Azure")
-
-
-    # LAUNCH SIMULATIONS
-    ssh.sendCommand("python " + destination_file_path)
-    print("Simulations launched on Machine")
-    # sms_instance.SimulationsStarted(i, login)
+    #
+    #
+    # # Copy the "HKS_LaunchDGP.py" file to the remote system
+    # original_file_path = os.path.join(login.Local_Repo_Directory,HKS_LaunchDGP)
+    # destination_file_path = Azure_Main_Directory + "/" + HKS_LaunchDGP
+    # ssh.copyfilesSCP(IP, 22, login.ADMIN_NAME, login.ADMIN_PSWD, original_file_path, destination_file_path)
+    # print(destination_file_path + " Copied to Azure")
+    #
+    #
+    # # LAUNCH SIMULATIONS
+    # ssh.sendCommand("python " + destination_file_path)
+    # print("Simulations launched on Machine")
+    # # sms_instance.SimulationsStarted(i, login)
 
 
 
 ###############################################  DEFINE THE MAIN FUNCTION  ###########################################
 
 def main(Local_Main_Directory_AGBD):
-    # ALL MULTITHREADING LOGIC TAKES PLACE HERE.  EACH OF THE FUNCTIONS REPRESENTS OPERATION ON A SINGLE ELEMENT
+    # This location is fixed and should not change for any cloud based sims using the LINE image template
+    Azure_Main_Directory = "/home/pferrer/new"
+
+    # Identify which files are going to be sent to azure
     filesForRemoteTransfer = getFilesForTransfer(Local_Main_Directory_AGBD)
-    zipFiles(Local_Main_Directory_AGBD,filesForRemoteTransfer)
+
+    # zip up the files
+    zippedfile = zipFiles(Local_Main_Directory_AGBD,filesForRemoteTransfer)
+
+    # Get the count and IP addresses of the currently assigned VM's
+    # vm_count = GET_VMCount()
+    vm_IP_List = GET_VMIP()
+    sms_main = sms.SMS()
+    login_main = Login()
+
+    # send all files to azure
+    sendFilesToAzureAndLaunch(Azure_Main_Directory, vm_IP_List[0], zippedfile)
